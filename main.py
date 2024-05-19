8477c7be605cbadc935360ebc04934fa1156438f
neydex <157636855+neydex@users.noreply.github.com>
2024-05-19 18:34:25 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from colorama import Fore
+import os
+
+print(Fore.RED + "  _  _ _____   _____  _____  __")
+print(" | \| | __\ \ / /   \| __\ \/ /")
+print(" | .` | _| \ V /| |) | _| >  < ")
+print(" |_|\_|___| |_| |___/|___/_/\_|")
+
+print("")
+
+print(Fore.LIGHTWHITE_EX + "Please enter your password.")
+pswd = input(Fore.RED + "Password: ")
+
+if pswd == "Xo3XLSGX1DG1ctIE":print(Fore.LIGHTGREEN_EX + "Password correct!"), print(""), input(Fore.LIGHTWHITE_EX + "Do you want to continue?"), os.system("start https://neydex.github.io/neydex")
+elif pswd == "SgdDggOoCMjGKn23": print(Fore.LIGHTGREEN_EX + "Password correct!"), print(""), input(Fore.LIGHTWHITE_EX + "Do you want to continue?"), os.system("start https://neydex.github.io/neydex")
+elif pswd == "lkVi0bqGSRpNYWlg": print(Fore.LIGHTGREEN_EX + "Password correct!"), print(""), input(Fore.LIGHTWHITE_EX + "Do you want to continue?"), os.system("start https://neydex.github.io/neydex")
+elif pswd == "4UKnfhC2PpDplTH7": print(Fore.LIGHTGREEN_EX + "Password correct!"), print(""), input(Fore.LIGHTWHITE_EX + "Do you want to continue?"), os.system("start https://neydex.github.io/neydex")
+elif pswd == "reset": print(Fore.LIGHTCYAN_EX + "Forgot Passwords?", Fore.LIGHTBLACK_EX + "4UKnfhC2PpDplTH7, lkVi0bqGSRpNYWlg, SgdDggOoCMjGKn23, Xo3XLSGX1DG1ctIE"), print(""), input(Fore.LIGHTWHITE_EX + "Press Enter to restart."), os.system("start main.exe")
+else : print(Fore.MAGENTA + "Password incorrect!"), print(""), input(Fore.RED + "Press Enter to close")
